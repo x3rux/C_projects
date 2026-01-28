c973ca911b87ac0597acd5f0cd9eaef50982f206
x3rux <f20231089@pilani.bits-pilani.ac.in>
2026-01-28 08:02:55 +0530

    basic functionality

--- CPClone/main.c
+++ CPClone/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Hello, World\n");
+void usage(){
+    printf("USAGE: <source> <destination>\n");
+}
+
+int main(int argc, const char** argv){
+    
+    if(argc <= 1){
+        usage();
+        exit(-1);
+    }
+
+    FILE* src = fopen(argv[1], "rb");
+    FILE* dest = fopen(argv[2], "wb");
+    if(src == NULL || dest == NULL){
+        printf("Error opening files!\n");
+        exit(-1);
+    }
+
+    char c;
+    while((c = getc(src)) != EOF){
+        putc(c, dest);
+    }
+
     return 0;
 }
