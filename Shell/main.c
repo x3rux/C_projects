5a797d45c20efd6c1e1f910855e006a6b512fdc2
x3rux <f20231089@pilani.bits-pilani.ac.in>
2026-02-15 21:10:11 +0530

    cd fixes

--- Shell/main.c
+++ Shell/main.c
@@ -33,27 +33,36 @@ int main(){
     while(1) {
         printf("myshell>>");
         nread = getline(&line, &size, stdin);
+        if (nread == EOF) break;
 
         char** args = tokenize(line);
+        if(args[0] == NULL){
+            free(args);
+            continue;
+        }
 
         if(strcmp(args[0],CD) == 0) {
-            if(chdir(args[1]) != 0){
+            if(args[1] == NULL){
+                fprintf(stderr, "myshell: expected argument to \"cd\"\n");
+            }
+            else if(chdir(args[1]) != 0){
                 fprintf(stderr, "ERROR: unable to change directory to %s\n", args[1]);
             }
+            free(args);
             continue;
         }
 
         pid_t pid = fork();
         if (pid == 0){
             execvp(args[0], args);
+            perror("myshell");
             exit(1);
         }
         else{
             wait(NULL);
         }
 
-
-        if (nread == EOF) break;
+        free(args);
     }
 
     return 0;
