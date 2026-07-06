7bd9480e236ba21f4bb54424c87de082a3590452
x3rux <f20231089@pilani.bits-pilani.ac.in>
2026-07-06 21:24:16 +0530

    matrix definitions

---
+++ footy-cv/include/Matrix.h
@@ -0,0 +1,18 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <cstddef>
+#include <stddef.h>
+
+typedef struct{
+    size_t rows;
+    size_t cols;
+    float *data;
+}Matrix;
+
+Matrix* matrix_allocate(size_t rows, size_t cols);
+void matrix_free(Matrix* m);
+void matrix_print(Matrix* m);
+
+#endif
+
